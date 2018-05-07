c3aa57990a3d190a923d39289000d942d2072efc
stemshell <amartin262@gmail.com>
2018-05-07 13:24:58 +1000

    added listings model; added carrierwave image uploader

---
+++ spec/views/listings/edit.html.erb_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "listings/edit", type: :view do
+  before(:each) do
+    @listing = assign(:listing, Listing.create!(
+      :title => "MyString",
+      :description => "MyText",
+      :image => "MyString",
+      :address => "MyString",
+      :price => "9.99"
+    ))
+  end
+
+  it "renders the edit listing form" do
+    render
+
+    assert_select "form[action=?][method=?]", listing_path(@listing), "post" do
+
+      assert_select "input[name=?]", "listing[title]"
+
+      assert_select "textarea[name=?]", "listing[description]"
+
+      assert_select "input[name=?]", "listing[image]"
+
+      assert_select "input[name=?]", "listing[address]"
+
+      assert_select "input[name=?]", "listing[price]"
+    end
+  end
+end
