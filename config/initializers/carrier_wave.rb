d1210921b5af7512b2224c73a1c031ca9d933549
stemshell <amartin262@gmail.com>
2018-05-09 20:55:12 +1000

    fixed bug with fog-aws by removing unecessary .fog file; implemented file uploads to s3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,14 +1,24 @@
 require 'carrierwave/orm/activerecord'
 
+# CarrierWave.configure do |config|
+#     config.fog_provider = 'fog/aws'                        # required
+#     config.fog_credentials = {
+#       :provider              => 'AWS',
+#       :aws_access_key_id     => ENV['S3_KEY'],
+#       :aws_secret_access_key => ENV['S3_SECRET'],
+#       :region                => 'ap-southeast-2' 
+#     }
+#     config.fog_directory  = 'stemshell'                                   # required
+#     config.fog_public     = false                                                 # optional, defaults to true
+#     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
+#   end
+
 CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'                        # required
     config.fog_credentials = {
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET'],
-      :region                => 'ap-southeast-2' 
+      provider:              'AWS',                        # required
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+      aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
     }
-    config.fog_directory  = 'stemshell'                                   # required
-    config.fog_public     = false                                                 # optional, defaults to true
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
-  end
+    config.fog_directory  = ENV["AWS_BUCKET"]              # required
+end
