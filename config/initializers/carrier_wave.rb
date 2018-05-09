1f99ed36d103de4fc3aca5ddce957492b1743bc8
stemshell <amartin262@gmail.com>
2018-05-09 19:48:15 +1000

    precautionary commit before removing dot-env gem

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,1 +1,14 @@
 require 'carrierwave/orm/activerecord'
+
+CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'                        # required
+    config.fog_credentials = {
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['S3_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET'],
+      :region                => 'ap-southeast-2' 
+    }
+    config.fog_directory  = 'stemshell'                                   # required
+    config.fog_public     = false                                                 # optional, defaults to true
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
+  end
