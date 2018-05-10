c10ec2b90cc3c8568fb290c77ce5409e1d85fa23
stemshell <amartin262@gmail.com>
2018-05-10 16:00:11 +1000

    added stripe gem and supporting files

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
 
+#payments
+gem 'stripe'
+
 # bootstrap styling
 gem 'bootstrap'
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   root 'listings#index'
+
+  #used for Stripe payments
+  resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
