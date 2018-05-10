f0e29ab77848bda25e195a87fb30c6090a6e3982
stemshell <amartin262@gmail.com>
2018-05-10 13:12:59 +1000

    had issues with previous commit; trying again

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -25,8 +25,7 @@ class ListingsController < ApplicationController
   # POST /listings
   # POST /listings.json
   def create
-    @listing = Listing.new(listing_params)
-    @listing.user = current_user
+    @listing = current_user.listings.build(listing_params)
 
     respond_to do |format|
       if @listing.save
